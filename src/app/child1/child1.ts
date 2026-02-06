fd6f1d725acce168e07b8a57108a551c8cefb845
xmikolay <s00254903@atu.ie>
2026-02-06 19:52:49 +0000

    child components code populated

--- src/app/child1/child1.ts
+++ src/app/child1/child1.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CounterService } from '../counter.service';
 
 @Component({
   selector: 'app-child1',
+  standalone: true,
   imports: [],
   templateUrl: './child1.html',
   styleUrl: './child1.css',
 })
 export class Child1 {
+  constructor(public counterService: CounterService){}
 
+  get count(){
+    return this.counterService.getCount();
+  }
 }
--- src/app/child2/child2.ts
+++ src/app/child2/child2.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CounterService } from '../counter.service';
 
 @Component({
   selector: 'app-child2',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './child2.css',
 })
 export class Child2 {
-
+  constructor(public counterService: CounterService){}
+  
+  get doubleCount(){
+    return this.counterService.doubleCount();
+  }
 }
